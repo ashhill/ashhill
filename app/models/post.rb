4712cdf5fa9d7b66b326061a2baeeef67b6691c4
Ash Hill <ash@marcatodigital.com>
2010-06-20 23:52:38 -0300

    mailer

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActionMailer::Base
+    def signup_notification
+      recipients "Ashley Hill <masa.ash@gmail.com>"
+      from       "My Forum "
+      subject    "Please activate your new account"
+      sent_on    Time.now
+      body       {}
+    end
+  end
