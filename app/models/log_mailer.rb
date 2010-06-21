20b7653ef3ef796e6e73cf4bee6975b32592d209
Ash Hill <ash@marcatodigital.com>
2010-06-21 20:24:14 -0300

    new log notification

--- app/models/log_mailer.rb
+++ app/models/log_mailer.rb
@@ -1,9 +1,16 @@
 class LogMailer < ActionMailer::Base
-  def signup_notification
-    recipients "Ashley Hill <masa.ash@gmail.com>"
-    from       "My Forum "
-    subject    "Please activate your new account"
-    sent_on    Time.now
-    body       {}
+  def new_message_noification(user, log)
+    setup_email(user, log)
   end
+
+  protected
+  def setup_email(user, log)
+    recipients "#{user.email}"
+    from      %("#{user.fullname}" <masa.ash@gmail.com>)
+    subject    "[AshHill.ca] #{log.title}"
+    body       :body => log.body, :current_user => log.user_id, :title => log.title
+    content_type "text/html"
+    sent_on    Time.now
+  end
+  
 end
